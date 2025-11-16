fe693d09ae625790fa8029de79006f570f87c6df
Chance <StaticElite05@gmail.com>
2025-11-16 03:46:06 -0500

    Better flake

--- flake.nix
+++ flake.nix
@@ -1,133 +1,85 @@
 # 🍳 cookin 👩‍🍳
 {
-  description = "python data aquisition flake";
+  description = "A better python data aquisition flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    utils.url = "github:numtide/flake-utils";
-    mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-proto.url = "github:notalltim/nix-proto";
+    nix-proto.inputs.nixpkgs.follows = "nixpkgs";
     mcap.url = "github:RCMast3r/py_mcap_nix";
+    mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
     asyncudp.url = "github:RCMast3r/asyncudp_nix";
     can_pkg_flake.url = "github:KSU-MS/ksu-ms-dbc/main";
-    nix-proto.url = "github:notalltim/nix-proto";
   };
 
-  outputs = { self, nixpkgs, utils, mcap-protobuf, mcap,
-     asyncudp, nix-proto, can_pkg_flake, ... }@inputs:
-    let
-      makePackageSet = pkgs: {
-        py_data_acq_pkg = pkgs.py_data_acq_pkg;
-        py_dbc_proto_gen_pkg = pkgs.py_dbc_proto_gen_pkg;
-        proto_gen_pkg = pkgs.proto_gen_pkg;
-        hytech_np = pkgs.hytech_np;
-        hytech_np_proto_py = pkgs.hytech_np_proto_py;
-        default = pkgs.py_data_acq_pkg;
-      };
+  # Everything your flake needs to produce what it provides
+  outputs = { self, nixpkgs, flake-utils, nix-proto, mcap, mcap-protobuf, asyncudp, can_pkg_flake, ... }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        py_dbc_proto_gen_overlay = final: prev: {
+          py_dbc_proto_gen_pkg = final.callPackage ./dbc_proto_gen_script.nix { };
+        };
+        py_data_acq_overlay = final: prev: {
+          py_data_acq_pkg = final.callPackage ./default.nix { };
+        };
+        proto_gen_overlay = final: prev: {
+          proto_gen_pkg = final.callPackage ./dbc_proto_bin_gen.nix { };
+        };
 
-      py_data_acq_overlay = final: prev: {
-        py_data_acq_pkg = final.callPackage ./default.nix { };
-      };
-      py_dbc_proto_gen_overlay = final: prev: {
-        py_dbc_proto_gen_pkg = final.callPackage ./dbc_proto_gen_script.nix { };
-      };
-      proto_gen_overlay = final: prev: {
-        proto_gen_pkg = final.callPackage ./dbc_proto_bin_gen.nix { };
-      };
+        nix_protos_overlays = nix-proto.generateOverlays' {
+          hytech_np = { proto_gen_pkg }:
+            nix-proto.mkProtoDerivation {
+              name = "hytech_np";
+              buildInputs = [ proto_gen_pkg ];
+              src = proto_gen_pkg.out + "/proto";
+              version = "1.0.0";
+            };
+        };
 
-      nix_protos_overlays = nix-proto.generateOverlays' {
-        hytech_np = { proto_gen_pkg }:
-          nix-proto.mkProtoDerivation {
-            name = "hytech_np";
-            buildInputs = [ proto_gen_pkg ];
-            src = proto_gen_pkg.out + "/proto";
-            version = "1.0.0";
-          };
-      };
-      my_overlays = [
-        py_dbc_proto_gen_overlay
-        py_data_acq_overlay
-        proto_gen_overlay
-        can_pkg_flake.overlays.default
-        mcap-protobuf.overlays.default
-        mcap.overlays.default
-        asyncudp.overlays.default
-      ] ++ nix-proto.lib.overlayToList nix_protos_overlays;
-      system = builtins.currentSystem;
-      x86_pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        # inherit system;
-        # system = builtins.currentSystem;
-        overlays = [ self.overlays.default ]
-          ++ nix-proto.lib.overlayToList nix_protos_overlays;
-      };
+        my_overlays = [
+          py_dbc_proto_gen_overlay
+          py_data_acq_overlay
+          proto_gen_overlay
+          mcap-protobuf.overlays.default
+          mcap.overlays.default
+          asyncudp.overlays.default
+          can_pkg_flake.overlays.default
+        ] ++ nix-proto.lib.overlayToList nix_protos_overlays;
 
-      arm_pkgs = import nixpkgs {
-        system = "aarch64-linux";
-        # inherit system;
-        # system = builtins.currentSystem;
-        overlays = [ self.overlays.default ]
-          ++ nix-proto.lib.overlayToList nix_protos_overlays;
-      };
+        # This combined with the flake-utils package abstracts what architecture you are building for
+        pkgs = import nixpkgs { 
+          inherit system;
+          overlays = my_overlays;
+        };
 
-      packageSets = {
-        "x86_64-linux" = makePackageSet x86_pkgs;
-        "aarch64-linux" = makePackageSet arm_pkgs;
-        # Add more systems as needed
-      };
-    in {
 
-      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+        # Shrimple dev shell to allow for local debug
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            jq
+            py_data_acq_pkg
+            py_dbc_proto_gen_pkg
+            proto_gen_pkg
+            can-utils
+            can_pkg
+          ];
 
-      packages = packageSets;
+          shellHook = ''
+            path=${pkgs.proto_gen_pkg}
+            bin_path=$path"/bin"
+            dbc_path=${pkgs.can_pkg}
+            export BIN_PATH=$bin_path
+            export DBC_PATH=$dbc_path
 
-      devShells.x86_64-linux.default = x86_pkgs.mkShell rec {
-        # Update the name to something that suites your project.
-        name = "ksu_daq";
-        packages = with x86_pkgs; [
-          jq
-          py_data_acq_pkg
-          py_dbc_proto_gen_pkg
-          proto_gen_pkg
-          can_pkg
-          cmake
-          can-utils
-        ];
+            echo -e "PYTHONPATH=$PYTHONPATH\nBIN_PATH=$bin_path\nDBC_PATH=$dbc_path\n" > .env
+          '';
+        };
 
-        # Setting up the environment variables you need during development.
-        shellHook = let icon = "f121";
-        in ''
-          path=${x86_pkgs.proto_gen_pkg}
-          bin_path=$path"/bin"
-          dbc_path=${x86_pkgs.can_pkg}
-          export BIN_PATH=$bin_path
-          export DBC_PATH=$dbc_path
+      in {
+        overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
 
-          echo -e "PYTHONPATH=$PYTHONPATH\nBIN_PATH=$bin_path\nDBC_PATH=$dbc_path\n" > .env
-          export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-        '';
-      };
-
-      devShells.x86_64-linux.ci = x86_pkgs.mkShell rec {
-        # Update the name to something that suites your project.
-        name = "ksu_daq";
-        packages = with x86_pkgs; [
-          # Development Tools
-          py_dbc_proto_gen_pkg
-          proto_gen_pkg
-          can_pkg
-          protobuf
-        ];
-
-        shellHook =
-        ''
-          path=${x86_pkgs.proto_gen_pkg}
-          bin_path=$path"/bin"
-          dbc_path=${x86_pkgs.can_pkg}
-          export BIN_PATH=$bin_path
-          export DBC_PATH=$dbc_path
-        '';
-
-      };
-
-    };
+        devShells.default = devShell;
+      }
+    );
 }
